4e8bc7ead97f113cbc57065da95671c3ac845562
Josué Izaguirre <flynn1411@gmail.com>
2019-04-24 17:17:46 -0600

    24/04/2019

---
+++ arbol.py
@@ -0,0 +1,44 @@
+from leaves import*
+
+class Arbol:
+
+	def __init__(self):
+		self.root = Node(Carpeta("/"))
+		self.currentNode= self.root
+		self.currentPath= self.getRoot()
+
+	def getRoot(self):
+		return self.root.getName()
+
+	def add(self, addValue, _type):
+		new= None
+
+		if _type == "-c":
+			new= Node(Carpeta(addValue))
+		else:
+			new= Node(Archivo(addValue))
+
+		self.currentNode.getValue().add(new)
+
+	def setCurrent
+
+
+def loadTreeStructure(self, path, treeWidget):
+	tree = Arbol()
+
+		# Para cada cosa que exista en path
+	try:
+		for file in os.listdir(path):
+			file_path= path + '/' + file
+
+			if os.path.isdir(file_path):
+				tree.add(file, "-c")
+				tree.setCurrent(file)
+				self.loadTreeStructure(file_path, treeItem)
+			
+			else:
+				tree.add(file, "-a")
+
+		except Exception as e:
+			print 'Exception: ', e
+			# Nada mas para evitar que no se muera
